f32acbc47640f83c75ee15b6a79288296a39d3bf
azurit <jozef@sudolsky.sk>
2024-03-30 14:04:06 +0100

    Update false-positive-report.lua

--- plugins/false-positive-report.lua
+++ plugins/false-positive-report.lua
@@ -1,3 +1,12 @@
+-- -----------------------------------------------------------------------
+-- OWASP CRS Plugin
+-- Copyright (c) 2022-2024 Core Rule Set project. All rights reserved.
+--
+-- The OWASP CRS plugins are distributed under
+-- Apache Software License (ASL) version 2
+-- Please see the enclosed LICENSE file for full details.
+-- -----------------------------------------------------------------------
+
 function string_strip(str)
 	return string.gsub(str, "^%s*(.-)%s*$", "%1")
 end
